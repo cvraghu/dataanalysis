d6139c428e6aa4d93c550da359d9e23b0524dbe7
cvraghu <cvraghu@hotmail.com>
2022-03-04 20:02:08 -0700

    Add files via upload
    Copy Millions of S3 files across buckets based on data range

---
+++ CopyS3FilesAcrossS3BucketsBasedOnDataRange.ps1
@@ -0,0 +1,8 @@
+﻿$source = "s3://source/"
+$destination = "s3://destination/"
+aws s3 ls $source | Select-String "2022-03-04" | foreach {
+    $filename = $_.Line.split(" ")[9]
+    $srcfile = $source + $filename
+    $dstfile = $destination + $filename
+    aws s3 cp $srcfile  $dstfile --recursive
+}
